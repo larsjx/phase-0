6dadb55e0784f4861e1e0525f70106e48b3def78
Lars Johnson <keepitlj@gmail.com>
2015-10-01 11:20:44 -0500

    Fix indentation on triangle challenge

--- week-4/triangle-side/my_solution.rb
+++ week-4/triangle-side/my_solution.rb
@@ -12,11 +12,11 @@
 
 def valid_triangle?(a, b, c)
 
-	if (a + b > c) && (a + c > b) && (b + c > a)
-        return true
-    else
-        return false
-    end
+  if (a + b > c) && (a + c > b) && (b + c > a)
+    return true
+  else
+    return false
+  end
 
 end
 
