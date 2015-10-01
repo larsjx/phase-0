6300a66a10d34deb8c7274cca83c134dfdf33415
Lars Johnson <keepitlj@gmail.com>
2015-10-01 08:53:52 -0500

    Add good-guess and simple-substring files

---
+++ week-4/simple-substrings/my_solution.rb
@@ -0,0 +1,16 @@
+# Simple Substrings
+
+# I worked on this challenge: By myself
+
+
+# Your Solution Below
+
+def welcome(address)
+  if address.include? "CA"
+    return "Welcome to California"
+  else
+    return "You should move to California"
+  end
+end
+
+# welcome(gets.chomp)
