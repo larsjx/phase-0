bb4076562a65422fb54b7a41404c67b59191c87b
Lars Johnson <keepitlj@gmail.com>
2015-10-14 19:15:18 -0500

    Add solution files for challenge 6.4

---
+++ week-6/attr/my_solution.rb
@@ -0,0 +1,63 @@
+
+#Attr METHODS
+
+# I WORKED ON THIS CHALLENGE: Lars Johnson
+
+# I spent [#] hours on this challenge.
+
+class NameData
+
+end
+
+
+class Greetings
+
+end
+
+
+=begin
+
+# REFLECTION
+__________________________________________
+RELEASE_1.rb QUESTIONS
+
+1. What are these methods doing?
+
+2. How are they modifying or returning the value of instance variables?
+
+
+__________________________________________
+RELEASE_2.rb QUESTIONS
+
+1. What changed between the last release and this release?
+
+2. What was replaced?
+
+3. Is this code simpler than the last?
+
+
+__________________________________________
+RELEASE_3.rb QUESTIONS
+
+1. What changed between the last release and this release?
+
+2. What was replaced?
+
+3. Is this code simpler than the last?
+
+
+__________________________________________
+RELEASE 6: FINAL CHALLENGE QUESTIONS
+
+1. What is a reader method?
+
+2. What is a writer method?
+
+3. What do the attr methods do for you?
+
+4. Should you always use an accessor to cover your bases? Why or Why not?
+
+5. What is confusing to you about these methods?
+
+
+=end
