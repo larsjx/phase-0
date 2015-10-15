6938f4dcb4d8d886cd782b459c677aa2986b0695
Lars Johnson <keepitlj@gmail.com>
2015-10-14 21:29:39 -0500

    Add new solution file for challenge 6.7

---
+++ week-6/credit-card/my_solution.rb
@@ -0,0 +1,26 @@
+
+# Class Warfare, Validate a Credit Card Number
+
+
+# I worked on this challenge with: Parminder Singh
+# I spent [#] hours on this challenge.
+
+# PSEUDOCODE
+
+# INPUT:
+# OUTPUT:
+
+# INITIAL SOLUTION
+
+# Don't forget to check on initialization for a card length
+# of exactly 16 digits
+
+class CreditCard
+
+end
+
+
+# Refactored Solution
+
+
+# Reflection
