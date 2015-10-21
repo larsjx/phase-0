aef1fcc59a705dab8972e7a2c14b3c0328a66c76
Lars Johnson <keepitlj@gmail.com>
2015-10-20 21:24:50 -0500

    Add eloquent.js solutions for 7.2

--- week-7/eloquent.js
+++ week-7/eloquent.js
@@ -5,18 +5,55 @@
 // Program Structure
 // Write your own variable and do something to it.
 
+var total = 320;
+total = total + 30;
+console.log(total);
 
+
+// Request User Input : Favorite Food
+// NOTE: THIS ONE MUST BE RUN IN THE CHROME CONSOLE
+
+// var favoriteFood = prompt(“What is your favorite food?”, “”);
+// console.log(“Hey That’s my favorite food too!”)
 
 
 // Complete one of the exercises: Looping a Triangle, FizzBuzz, or Chess Board
 
+// ########### LOOPING A TRIANGLE
+
+var pound = "";
+for (i = 0; i < 7; i++) {
+    pound += "#";
+    console.log(pound)
+}
 
 
 // Functions
 
 // Complete the `minimum` exercise.
 
+function min(x, y) {
+    if (x == y)
+        console.log("Numbers are equal");
+    else if (x < y)
+        console.log(x);
+    else
+        console.log(y);
+}
+
+min(2, 4)
+
 
 
 // Data Structures: Objects and Arrays
 // Create an object called "me" that stores your name, age, 3 favorite foods, and a quirk below.
+
+
+var me = {
+  name: "Lars",
+  age: "48",
+  favorite_foods: ["Chinese food", "steak", "gelato"],
+  quirk: "I've gone hang gliding in Rio"
+};
+
+console.log(me.quirk)
