318f287175cad9383ebf7463897f0c10806c3635
Lars Johnson <keepitlj@gmail.com>
2015-10-22 08:54:40 -0500

    Add game.js shell file

---
+++ week-7/game.js
@@ -0,0 +1,55 @@
+/*
+
+  SOLO CHALLENGE 7.7 : DESIGN A BASIC GAME
+
+  *** THIS IS A SOLO CHALLENGE ***
+
+###################################################################
+
+  YOUR MISSION DESCRIPTION:
+  OVERALL MISSION:
+  GOALS:
+  CHARACTERS:
+  OBJECTS:
+  FUNCTIONS:
+
+###################################################################
+
+  PSEUDOCODE
+
+   INPUT:
+  OUTPUT:
+
+
+*/
+// #################################################################
+// INITIAL SOLUTION
+
+
+
+
+
+
+// #################################################################
+// REFACTORED SOLUTION
+
+
+
+
+// #################################################################
+/* REFLECTION
+
+  1. What was the most difficult part of this challenge?
+
+
+  2. What did you learn about creating objects and functions that interact with one another?
+
+
+  3. Did you learn about any new built-in methods you could use in your refactored solution?
+     If so, what were they and how do they work?
+
+
+  4. How can you access and manipulate properties of objects?
+
+
+*/
