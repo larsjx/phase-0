ad1dde3af5b710077ce07d450d92f86762b70da0
Lars Johnson <keepitlj@gmail.com>
2015-10-22 08:25:26 -0500

    Create JS Olympics file for 7.6

---
+++ week-7/javascript_olympics.js
@@ -0,0 +1,57 @@
+/*
+
+  PEER PARING CHALLENGE 7.6: JAVASCRIPT OLYMPICS
+
+  WE PAIRED ON THIS CHALLENGE: Becca Nelson & Lars Johnson
+  THIS CHALLENGE TOOK US: ___ Hours
+
+*/
+// ###########################################################
+// WARM UP
+
+
+
+
+// ###########################################################
+// BULK UP
+
+
+
+
+// ###########################################################
+// JUMBLE YOUR WORDS
+
+
+
+
+// ###########################################################
+// 2, 4, 6, 8! WHO DO YOU APPRECIATE?
+
+
+
+
+// ###########################################################
+// WE BUILT THIS CITY
+
+
+/* ________________________________________________________________________________________
+// DRIVER TEST CODE
+
+var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
+console.log(michaelPhelps.constructor === Athlete)
+console.log(michaelPhelps.name + " " + michaelPhelps.sport + " " + michaelPhelps.quote)
+
+/*
+/* ###########################################################
+// REFLECTION
+
+  1. What JavaScript knowledge did you solidify in this challenge?
+
+
+  2. What are constructor functions?
+
+
+  3. How are constructors different from Ruby classes (in your research)?
+
+
+*/
