c00d053ad4801cd44f1ce1e62a013ef823c8c5fc
Lars Johnson <keepitlj@gmail.com>
2015-10-22 14:28:38 -0500

    Add initial solutions for 7.6

--- week-7/javascript_olympics.js
+++ week-7/javascript_olympics.js
@@ -7,35 +7,72 @@
 
 */
 // ###########################################################
-// WARM UP
-
-
-
-
-// ###########################################################
 // BULK UP
 
+function Athlete(name, event) {
+  this.name = name,
+  this.event = event
+}
 
+var sarah = new Athlete("Sarah Hughes", "Ice Skating");
+var michael = new Athlete("Michael Jordan", "Basketball");
+
+var athleteArray = [sarah, michael];
+console.log(athleteArray);
+
+sarah.win = sarah.name + " won at " + sarah.event;
+
+console.log(sarah);
 
 
 // ###########################################################
 // JUMBLE YOUR WORDS
 
+function stringReverse(string) {
+  var stringArray = string.split("");
+  stringArray = stringArray.reverse();
+  console.log(stringArray.join(""));
+}
 
+stringReverse("Jumble your words!")
 
 
 // ###########################################################
 // 2, 4, 6, 8! WHO DO YOU APPRECIATE?
 
+function checkEven(value, index) {
+  if (value % 2 === 0) {
+    return true;
+  }
+}
 
+var numbers = [1,2,3,4,5];
+var evenNumbers = numbers.filter(checkEven);
+console.log(evenNumbers);
+
+// OR THIS VERSION OF 2, 4, 6, 8!
+
+function evenNumbers(numbersArray) {
+  var newArray = [];
+  numbersArray.forEach(function(n) {
+    if (n % 2 === 0) {
+      newArray.push(n);
+    }
+  })
+  console.log(newArray);
+}
 
 
 // ###########################################################
 // WE BUILT THIS CITY
 
 
-/* ________________________________________________________________________________________
-// DRIVER TEST CODE
+function Athlete(name, age, sport, quote) {
+  this.name = name,
+  this.age = age,
+  this.sport = sport,
+  this.quote = quote
+}
 
 var michaelPhelps = new Athlete("Michael Phelps", 29, "swimming", "It's medicinal I swear!")
 console.log(michaelPhelps.constructor === Athlete)
