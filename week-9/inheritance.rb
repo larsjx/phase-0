1569f990ad07782084eb6d4f909d90322fd25dae
Lars Johnson <keepitlj@gmail.com>
2015-11-05 16:23:55 -0600

    Add second solution to 9.5

--- week-9/inheritance.rb
+++ week-9/inheritance.rb
@@ -1,3 +1,4 @@
+=begin
 ################################################################################
 # INTRODUCTION TO INHERITANCE
 #
@@ -6,12 +7,14 @@
 ################################################################################
 # PSEUDOCODE
 
+   INPUT: A list of students comprising each local cohort
+  OUTPUT:
 
 
 
 ################################################################################
-# INITIAL SOLUTION
-
+# INITIAL SOLUTION WITH ARRAY DATA STRUCTURE
+=end
 =begin
 
 class GlobalCohort
@@ -108,70 +111,71 @@ copperheads.num_students
 =end
 
 ################################################################################
-# REFACTORED SOLUTION
+# INITIAL SOLUTION WITH HASH DATA STRUCTURE
 
-class GlobalCohort
+class Cohort
 
-  attr_reader :cities, :ny_students, :ch_students, :sf_students, :name, :p0
-  attr_accessor :students, :p1, :p2, :p3, :grad
+  attr_reader :cities, :ny_names, :ch_names, :sf_names, :name, :p0, :check_date
+  attr_accessor :names
 
-    def initialize (cities, ny_students, ch_students, sf_students, p0)
-      require "date"
-      @today = Date.today
+    def initialize (cities, ny_names, ch_names, sf_names, p0)
       @cities = cities
-      @students = { ny: ny_students, ch: ch_students, sf: sf_students }
-      @email = []
+      @names = {"New York" => ny_names, "Chicago" => ch_names, "San Fran" => sf_names }
+      require "date"
+      @day = Date.today
       @p0 = Date.parse(p0)
       @p1 = @p0 + 63
       @p2 = @p1 + 21
       @p3 = @p2 + 21
       @grad = @p3 + 21
+      @email = []
     end
 
   def create_email
-     @students.each_key { |key|
-        @students[key].each { |element|
+     @names.each_key { |key|
+        @names[key].each { |element|
           @email.push(element + "@dbc.com")
         }
       }
       return @email
    end
 
-   def num_students
-    @num_students = 0
-      @students.each_key { |key|
-        @num_students += @students[key].length
+   def students
+    @students = 0
+      @names.each_key { |key|
+        @students += @names[key].length
       }
-      return @num_students
+      return @students
     end
 
     def add(city, name)
-      @students[city] << name
-      @students[city].flatten!
+      @names[city] << name
+      @names[city].flatten!
     end
 
     def remove(city, name)
-      @students[city].each do |element|
+      @names[city].each do |element|
         if element == name
-          @students[city].delete(element)
+          @names[city].delete(element)
         end
       end
     end
 
-    def phase
-      if @today < @p1
-        return "Phase-0"
-      elsif @today > @p1  &&  Date.today <@p2
-        return "Phase-1"
-      elsif @today > @p2  &&  Date.today <@p3
-        return "Phase-2"
-      elsif @today > @p3  &&  @grad
-        return "Phase-3"
+    def what_phase(check_date)
+      check_date != "" ? @day = Date.parse(check_date) : @day = Date.today
+      if @day < @p1
+        return "Currently in Phase-0"
+      elsif @day > @p1  &&  @day < @p2
+        return "Currently in Phase-1"
+      elsif @day > @p2  &&  @day < @p3
+        return "Currently in Phase-2"
+      elsif @day > @p3  &&  @grad
+        return "Currently in Phase-3"
       end
     end
 
-    def graduated
-      if @today > @grad
+    def graduating
+      if @day > @grad
         return "Graduated"
       else
         return "Graduating on #{@grad}"
@@ -180,22 +184,23 @@ class GlobalCohort
 end
 
 
-class LocalCohort < GlobalCohort
+class LocalCohort < Cohort
 
   attr_accessor :students
 
     def initialize (students)
-      @students = students
-      p @students
+      @names = students
     end
 
     def add(student_name)
-      @students << student_name
+      @names << student_name
     end
 
     def remove(student_name)
-      @students.each { |element|
-        element == student_name ? @students.delete(element) : x=x
+      @names.each { |element|
+        if element == student_name
+        @names.delete(element)
+        end
       }
     end
 
@@ -206,29 +211,27 @@ cities = ["New York", "Chicago", "San Francisco"]
 ny = LocalCohort.new(["Bruno", "Marie-France", "Eunice", "Jon"])
 ch = LocalCohort.new(["Parminder", "Josh", "Becca", "Gabrielle"])
 sf = LocalCohort.new(["Nicole","Paul", "Jeremy", "Danielle"])
-
-copperheads = GlobalCohort.new(cities, ny.students, ch.students, sf.students, "07-09-2015")
+copperheads = Cohort.new(cities, ny.names, ch.names, sf.names, "07-09-2015")
 
 ny.add("Hilary")
 ny.remove("Bruno")
-p ny.students
 
-p copperheads.students
-
-copperheads.add(:ny, "Bruno")
-copperheads.remove(:sf, "Nicole")
-p copperheads.students
-
+p copperheads.names
+puts
+copperheads.add("New York", "Bruno")
+copperheads.remove("San Fran", "Nicole")
+puts
+p copperheads.names
+puts
 p copperheads.create_email
-
-p copperheads.num_students
-
-p copperheads.phase
-
-p copperheads.graduated
-
-
-
+puts
+p copperheads.students
+p copperheads.what_phase("03-03-2016")
+p copperheads.graduating
+puts
+p ny.names
+p sf.names
+p ch.names
 
 =begin
 ################################################################################
