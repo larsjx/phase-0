f7b52fda4fe51ee1e23258eda625f5446fdaa9a4
Lars Johnson <keepitlj@gmail.com>
2015-11-07 11:23:48 -0600

    Add reflection to challenge 9.3.2

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -1,26 +1,38 @@
-// U3.W9:JQuery
+/*
+###################################################################################################
+CHALLENGE 9.3.2 - INTRODUCTION TO JQUERY
 
+WE WORKED ON THIS CHALLENGE: Bruno Zatta & Lars Johnson
 
-// I worked on this challenge [by myself, with: ].
-// This challenge took me [#] hours.
+PLEASE NOTE: WE ALSO COMPLETED CHALLENGE 9.3.1: MANIPULATING THE DOM IN JS (IN A SEPARATE DIRECTORY)
+
+*/
 
 $(document).ready(function(){
 
-//RELEASE 0:
-  //link the image
+/*
+###################################################################################################
+RELEASE 0 - LINK THE IMAGE FILE
+*
 
-//RELEASE 1:
-
-  //Link this script and the jQuery library to the jQuery_example.html file and analyze what this code does.
+/*
+###################################################################################################
+RELEASE 1 -
+LINK THIS SCRIPT & THE JQUERY LIBRARY TO THE JQUERY-EXAMPLE.HTML FILE & ANALYZE WHAT THIS CODE DOES
+*/
 
 $('body').css({'background-color': 'pink'})
 
-//RELEASE 2:
-  //Add code here to select elements of the DOM
+/*
+###################################################################################################
+RELEASE 2 -
+THERE WAS NO CONTENT FOR RELEASE 2 IN THIS CHALLENGE
 
+###################################################################################################
+RELEASE 3 -
+ADD CODE HERE TO MODIFY THE CSS AND HTML OF DOM ELEMENTS
+*/
 
-//RELEASE 3:
-  // Add code here to modify the css and html of DOM elements
 var bodyElement = $('body');
 var outerHeader = $('body h1').filter(':first');
 var innerHeader = $('.mascot h1');
@@ -30,8 +42,11 @@ var img = $('img');
 outerHeader.css({'color': 'red', 'border-bottom': '5px solid red', 'visibility': 'hidden'});
 innerHeader.html('Copperheads');
 
-//RELEASE 4: Event Listener
-  // Add the code for the event listener here
+/*
+###################################################################################################
+RELEASE 4 -
+ADD THE CODE FOR THE EVENT LISTENER HERE
+*/
 
 $('img').on('mouseover', function(){
     // e.preventDefault()
@@ -43,8 +58,11 @@ $('img').on('mouseout', function(){
     $(this).attr('src', 'dbc_logo.png')
   })
 
-//RELEASE 5: Experiment on your own
-
+/*
+###################################################################################################
+RELEASE 5 -
+EXPERIMENT ON YOUR OWN
+*/
 
 img.click(function() {
   $('img').animate({height: "toggle"}, 5000 //function() {
@@ -54,3 +72,29 @@ img.click(function() {
 
 
 })  // end of the document.ready function: do not remove or write DOM manipulation below this.
+
+/*
+###################################################################################################
+REFLECTION
+
+1. What is jQuery?
+
+    jQuery is a JavaScript library that makes writing/executing JavaScript easier and more concise.
+    It does this by bundling many common JavaScript tasks into a framework fo easy to use methods.
+
+2. What does jQuery do for you?
+
+    jQuery greatly simplifies the creation of JavaScript enabled solutions.
+
+3. What did you learn about the DOM while working on this challenge?
+
+    Everything I know about manipulating the DOM in Javascript (which isn't very much yet), comes
+    from working on this jQuery challenge and its sister challenge 9.3.1 - Manipulating the DOM
+    with JavaScript.
+
+    I understand that jQuery will make this much easier than writing standard JavaScript, but I
+    haven't really seen that aspect of yet.
+
+PLEASE NOTE: WE ALSO COMPLETED CHALLENGE 9.3.1: MANIPULATING THE DOM IN JS (IN A SEPARATE DIRECTORY)
+
+*/
